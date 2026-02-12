616f75176835cb3e9e1268c49db9c896f690225d
John Colton <johnlouisanthony@gmail.com>
2026-02-12 09:17:37 -0600

    initial single digit simulated version of output_register.v is functional

--- computer/output_register.v
+++ computer/output_register.v
@@ -1,39 +1,59 @@
 module output_register(
     input clk,
-    input fast_clk,
+    //input fast_clk,
 
-    output reg [6:0] digit,
-    //output reg [1:0] display,
+    output reg [6:0] display,
     
     input clear,
-    input [7:0] bus
+    input [7:0] bus,
+    input input_en
     );
     
-    reg [7:0] data_in;
+    reg [7:0] data;
+    reg [3:0] digit0, digit1, digit2;
     
     //initialize space for the conversion
     reg [19:0] conversion;
     
     always @(posedge clk) begin
-        if (data_in) begin
-            data_in <= bus;
+        if (input_en) begin
+            data <= bus;
         end
     end
     
+    integer i;
     always @(*) begin
-        conversion[7:0] = data_in;
-        while (conversion[7:0] > 7'd0) begin
-            conversion = conversion << 1;
-            if (conversion[11:8] => 4'd5) begin
+        conversion[7:0] = data;
+        for (i = 0; i < 8; i++) begin
+            if (conversion[11:8] > 4'd4) begin
                 conversion[11:8] = conversion[11:8] + 3;
             end
-            if (conversion[15:12] => 4'd5) begin
+            if (conversion[15:12] > 4'd4) begin
                 conversion[15:12] = conversion[15:12] + 3;
             end
-            if (conversion[19:16] => 4'd5) begin
+            if (conversion[19:16] > 4'd4) begin
                 conversion[19:16] = conversion[19:16] + 3;
             end
+            conversion = conversion << 1;
         end
+        digit0 = conversion[11:8];
+        digit1 = conversion[15:12];
+        digit2 = conversion[19:16];
+    end
+    
+    always @(*) begin
+        case (digit0)
+            0: display = 7'b1111110;
+            1: display = 7'b0110000;
+            2: display = 7'b1101101;
+            3: display = 7'b1111001;
+            4: display = 7'b0110011;
+            5: display = 7'b1011011;
+            6: display = 7'b1011111;
+            7: display = 7'b1110000;
+            8: display = 7'b1111111;
+            9: display = 7'b1111011;
+        endcase
     end
     
     /*always @(posedge fast_clk) begin
--- computer/tb_computer.v
+++ computer/tb_computer.v
@@ -10,7 +10,10 @@ reg reg1_clr;
 reg [7:0] bus_data;
 reg bus_enable;
 
+wire [6:0] digit_test;
+reg output_r;
 
+/*
 eightbit_computer computer(
   .bus (bus),
   .clk (clk),
@@ -18,16 +21,32 @@ eightbit_computer computer(
   .reg1_data_out (reg1_out),
   .reg1_clr (reg1_clr)
 );
+*/
 
-/*
+output_register output_test(
+  .clk (clk),
+  .display (digit_test),
+  .clear (reset),
+  .bus (bus),
+  .input_en (output_r)
+);
+
 initial begin
   clk = 1'b0;
   forever #1 clk = ~clk;
 end
-*/
 
 assign bus = bus_enable ? bus_data : 8'bz;
 
+initial begin
+  bus_enable = 1'b1;
+  bus_data = 8'd118;
+  output_r = 1'b1;
+  #2
+  $display("Time: %h, Output: %b", $time, digit_test);
+end 
+
+/*
 initial begin
   clk = 1'b0;
   bus_enable = 1'b1;
@@ -55,7 +74,7 @@ initial begin
   #1
 
   $display("Time: %d,  Bus: %h, reg out: %b", $time,  bus, reg1_out);
-end
+end*/
 
 
 endmodule
