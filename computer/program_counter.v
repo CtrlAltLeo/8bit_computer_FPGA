0dc7ec142b194cf4066c6a20ae054c8d8afcfe9d
L Devick <71308358+CtrlAltLeo@users.noreply.github.com>
2026-02-17 09:00:41 -0600

    Change led output type to wire and negate counter
    Changed led output from reg to wire and updated assignment to negate counter.

--- computer/program_counter.v
+++ computer/program_counter.v
@@ -9,7 +9,7 @@ input c_in;
 input clear;
 input reset;
 
-output reg [3:0] led;
+output wire [3:0] led;
 
 reg [3:0] counter;
 
@@ -22,8 +22,10 @@ begin
 	else if (enable)
 		counter <= counter + 1;
 		
-	led <= counter;
+	//led <= ~counter;
 end
+
+assign led = ~counter; //led is a wire and needs to be driven continuously. Negated because LEDS are active low
 
 assign bus = c_out ? {4'b0000, counter} : 8'bz;
 
