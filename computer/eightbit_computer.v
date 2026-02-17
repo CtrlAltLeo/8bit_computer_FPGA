7255572bae96e93fe7bf7e7b21c91d6428d56121
L Devick <71308358+CtrlAltLeo@users.noreply.github.com>
2026-02-17 08:59:36 -0600

    Adding Program Counter Module

--- computer/eightbit_computer.v
+++ computer/eightbit_computer.v
@@ -1,5 +1,6 @@
 // "Frame" of the computer were parts are mounted
 module computer(input clock,
+					 input reset_btn,
 					inout wire [7:0] bus,
 					output MI, // MAR In
 					output RI, //RAM In
@@ -9,7 +10,7 @@ module computer(input clock,
 					output [6:0] SS_Out, // 7 segment display out
 					output [3:0] SS_Sel, // 7 segment display selector
 					output NEG, // Add minus sign to display
-					output [3:0] led
+					output wire [3:0] led
 					);
 					
 wire Load_Instruction;
@@ -26,11 +27,27 @@ wire Counter_Clear;
 
 wire Reset;
 
+// Temp Drivers
+assign Enable_Counter = 1;
+
 // Program Counter
+program_counter pc(
+	.clock(clock), 
+	.bus(bus), 
+	.enable(Enable_Counter), 
+	.c_out(Write_Counter), 
+	.c_in(Counter_In), 
+	.clear(Counter_Clear), 
+	.reset(Reset), 
+	.led(led)
+);
 
 // Instruction Register + Control Logic
 
 // A, B and ALU				
 
-// Output Register			
+// Output Register
+
+assign Reset = ~reset_btn;			
+
 endmodule
