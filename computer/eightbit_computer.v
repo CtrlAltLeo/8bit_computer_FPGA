a8c66a11bb2b5ba75195a6de9180bce5c0448169
L Devick <71308358+CtrlAltLeo@users.noreply.github.com>
2026-02-19 09:16:27 -0600

    Added temp drivers for testing PC

--- computer/eightbit_computer.v
+++ computer/eightbit_computer.v
@@ -10,7 +10,8 @@ module computer(input clock,
 					output [6:0] SS_Out, // 7 segment display out
 					output [3:0] SS_Sel, // 7 segment display selector
 					output NEG, // Add minus sign to display
-					output wire [3:0] led
+					output wire [3:0] led,
+					input [3:0] pc_drive //TEMP FOR TESTING PC
 					);
 					
 wire Load_Instruction;
@@ -28,14 +29,18 @@ wire Counter_Clear;
 wire Reset;
 
 // Temp Drivers
-assign Enable_Counter = 1;
+assign Enable_Counter = pc_drive[0];
+assign Counter_Out = pc_drive[1];
+assign Counter_In = pc_drive[2];
+assign Counter_Clear = pc_drive[3];
+
 
 // Program Counter
 program_counter pc(
 	.clock(clock), 
 	.bus(bus), 
 	.enable(Enable_Counter), 
-	.c_out(Write_Counter), 
+	.c_out(Counter_Out), 
 	.c_in(Counter_In), 
 	.clear(Counter_Clear), 
 	.reset(Reset), 
