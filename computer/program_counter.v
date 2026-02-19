bffc0cba754c10319044b9ab98c37f7e7baebf98
L Devick <71308358+CtrlAltLeo@users.noreply.github.com>
2026-02-19 09:17:53 -0600

    Fix led assignment in program_counter.v
    Corrected assignment of led to ensure proper functionality.

--- computer/program_counter.v
+++ computer/program_counter.v
@@ -21,11 +21,9 @@ begin
 		counter <= bus[3:0];
 	else if (enable)
 		counter <= counter + 1;
-		
-	//led <= ~counter;
 end
 
-assign led = ~counter; //led is a wire and needs to be driven continuously. Negated because LEDS are active low
+assign led = ~counter;
 
 assign bus = c_out ? {4'b0000, counter} : 8'bz;
 
