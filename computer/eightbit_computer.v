37e89b2bd44908c9e6b73578299653bb7f255e72
John Colton <johnlouisanthony@gmail.com>
2026-02-24 08:19:00 -0600

    added output_register to eightbit_computer

--- computer/eightbit_computer.v
+++ computer/eightbit_computer.v
@@ -1,17 +1,18 @@
 // "Frame" of the computer were parts are mounted
 module computer(input clock,
-					 input reset_btn,
-					inout wire [7:0] bus,
-					output MI, // MAR In
-					output RI, //RAM In
-					output RO, //RAM out
-					output HLT, // Halt Clock
-					output CLR, // Clear MAR
-					output [6:0] SS_Out, // 7 segment display out
-					output [3:0] SS_Sel, // 7 segment display selector
-					output NEG, // Add minus sign to display
-					output wire [3:0] led
-					);
+				input reset_btn,
+				input fast_clock, //for output_register
+				inout wire [7:0] bus,
+				output MI, // MAR In
+				output RI, //RAM In
+				output RO, //RAM out
+				output HLT, // Halt Clock
+				output CLR, // Clear MAR
+				output [6:0] SS_Out, // 7 segment display out
+				output [2:0] SS_Sel, // 7 segment display selector
+				output NEG, // Add minus sign to display
+				output wire [3:0] led
+				);
 					
 wire Load_Instruction;
 wire Load_A;
@@ -24,6 +25,8 @@ wire Enable_Counter;
 wire Counter_Out;
 wire Counter_In;
 wire Counter_Clear;
+wire Output_Clear;
+wire Output_In;
 
 wire Reset;
 
@@ -47,6 +50,15 @@ program_counter pc(
 // A, B and ALU				
 
 // Output Register
+output_register or(
+	.clk(clock),
+	.fast_clk(fast_clock),
+	.clear(Output_Clear),
+	.bus(bus),
+	.input_en(Output_In),
+	.display(SS_Out),
+	.display_en(SS_Sel)
+);
 
 assign Reset = ~reset_btn;			
 
