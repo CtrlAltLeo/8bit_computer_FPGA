e0a1440b2c718cb00fbe09ea874f497d3869c79a
Leo Devick <leodevick@gmail.com>
2026-03-12 08:10:34 -0500

    first chunk

--- computer/control_logic.v
+++ computer/control_logic.v
@@ -1,6 +1,7 @@
 module control_logic(
     input clock,
     input reset,
+    inout [7:0] bus,
     //control signals
     output HLT,
     output MI,
@@ -18,18 +19,25 @@ module control_logic(
     output BI,
     output DI
     );
-    
+   
     reg [7:0] instruction;
 
     //control counter
-    reg [3:0] count;
+    reg [4:0] count;
     reg count_clear = 1'b0;
 
     always @(negedge clock) begin
-        if (count_clear)
+        if (count == 2'b0111)
             count <= 4'b0;
         else
             count <= count + 1;
+    end
+
+    always @(*) begin
+      if (count[2:0] == 0) begin //PC OUT, MAR IN
+
+      end
+      
     end
     
 endmodule
